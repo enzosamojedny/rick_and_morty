7321054b9cd09656bf6b58361930fec364d4ff30
enzosamojedny <enzosamojedny@gmail.com>
2023-09-08 17:33:20 -0300

    bugs

--- Server/src/controllers/getCharById.js
+++ Server/src/controllers/getCharById.js
@@ -1,27 +1,12 @@
 const axios = require("axios");
-const express = require("express");
-function getCharById(res, id) {
-  axios(`https://rickandmortyapi.com/api/character/${id}`)
+const URL = "https://rickandmortyapi.com/api/character";
+
+function getCharById({ req, res }) {
+  const { id } = Params; //?aca la cagamos
+
+  axios(`URL/${id}`)
     .then((response) => response.data)
-    .then(({ name, gender, species, origin, image, status }) => {
-      const character = {
-        id,
-        name,
-        gender,
-        species,
-        origin: origin.name,
-        image,
-        status,
-      };
-      res.writeHead(200, { "Content-Type": "application/json" });
-      return res.end(JSON.stringify(character));
-    })
-    .catch((error) => {
-      return res
-        .writeHead(500, { "Content-Type": "text/plain" })
-        .end(error.message);
-    });
+    .then({ status, name, species, origin, gender, image });
 }
-module.exports = {
-  getCharById,
-};
+
+module.exports = { getCharById };
